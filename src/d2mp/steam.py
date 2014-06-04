26ae4e5b355a286ab30bb21563c3d79a62c5aa97
Dimitri Korsch <korschdima@yahoo.de>
2014-06-04 13:50:39 +0200

    added requirements. finished steam functions

--- src/d2mp/steam.py
+++ src/d2mp/steam.py
@@ -4,6 +4,7 @@ Created on 01.06.2014
 @author: Schleppi
 '''
 from PyQt4.Qt import QDesktopServices, QUrl
+import psutil
 
 def command(cmd):
     return QDesktopServices.openUrl(QUrl("steam://%s" %(cmd)))
@@ -11,15 +12,24 @@ def command(cmd):
 def launch_dota():
     return command("run/570")
 
+def get_dota_process():
+    for proc in psutil.process_iter():
+        name = proc.as_dict(['name'])['name'] or ''
+        if "dota" in name.lower(): return proc
+    return None
+
 def is_dota_running():
-    return False
+    return get_dota_process() is not None
 
 def kill_dota():
-    pass
-
+    dota_proc = get_dota_process()
+    if dota_proc is not None: dota_proc.kill()
+    
 def connect_dota(address, port):
-    pass
+    command("connect/%s:%s" %(address, port))
+
 
 def spectate(address, port):
-    pass
-
+    kill_dota()
+    command("rungameid/570//+connect_hltv %s:%s" %(address, port))
+        
