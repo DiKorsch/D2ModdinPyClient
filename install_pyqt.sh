b666aa5083beec5e18e9378bf1ca226e27bf2b79
Dimitri Korsch <korschdima@yahoo.de>
2014-06-07 21:49:43 +0200

    modified build scripts and requirements

--- install_pyqt.sh
+++ install_pyqt.sh
@@ -2,6 +2,6 @@ cd ..
 wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11/PyQt-x11-gpl-4.11.tar.gz
 tar -xvf PyQt-x11-gpl-4.11.tar.gz
 cd PyQt-x11-gpl-4.11
-python configure-ng.py --confirm-license -e QtCore -e QtGui -e QtNetwork
+python configure-ng.py --confirm-license --sip-incdir=../sip-4.16/siplib -e QtCore -e QtGui -e QtNetwork
 make
 sudo make install
